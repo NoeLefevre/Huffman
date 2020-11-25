221cea8484849d8c57adccc6bd14bc6f6df17b40
efreims <64196173+efreims@users.noreply.github.com>
2020-11-25 15:46:13 +0100

    Delete Arbre_huffman.h

--- Projet Huffman/Arbre Huffman/Arbre_huffman.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARBRE_HUFFMAN_H_INCLUDED
-#define ARBRE_HUFFMAN_H_INCLUDED
-#include "../Stucture/Structure.h"
-#include "../File/File.h"
-Node* trees_copy_tree(Node* tree);
-Node* create_node(int val,char charactere);
-Queue *create_queue_node(Element *l);
-Node * arbre_huffman(Element *l);
-#endif // ARBRE_HUFFMAN_H_INCLUDED
