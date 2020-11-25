3826d35e6e2eb53e38a8ce121d6a123c160666a3
efreims <64196173+efreims@users.noreply.github.com>
2020-11-25 15:48:11 +0100

    Delete Encodage.h

--- Projet Huffman/Encodage/Encodage.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENCODAGE_H_INCLUDED
-#define ENCODAGE_H_INCLUDED
-#include "../Stucture/Structure.h"
-FILE *encodage(FILE *fichier,Node_d *tree);
-char* nbr_recherche_arbre_recherche(Node_d *tree,char c);
-
-#endif // ENCODAGE_H_INCLUDED
