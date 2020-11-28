74d0d48b21671abbe50376d4f88f3a3561591cf1
efreims <64196173+efreims@users.noreply.github.com>
2020-11-28 12:16:41 +0100

    Update Dictionnaire.h

--- Projet Huffman Opti/Dictionnaire/Dictionnaire.h
+++ Projet Huffman Opti/Dictionnaire/Dictionnaire.h
@@ -1,11 +1,8 @@
 #ifndef DICTIONNAIRE_H_INCLUDED
 #define DICTIONNAIRE_H_INCLUDED
 #include "../Stucture/Structure.h"
-#include "../Pïle/Pile.h"
-void ajouter_ln_fin(List_node** liste, List_node* el);
+#include "../PÃ¯le/Pile.h"
 List_node *create_el_node(Node *n);
-Node *extract(List_node *liste);
-void suppr_fin(List_node **l);
 int list_position(List_node *l, Node* p);
 Node_d* create_node2(char c,char* code);
 int depth(Node* tree);
