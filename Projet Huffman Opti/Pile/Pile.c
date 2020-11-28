703c935df574379f240dc5cbf4f24a5c9fe47e84
NoeLefevre <73334531+NoeLefevre@users.noreply.github.com>
2020-11-28 12:32:26 +0100

    Update Pile.c

--- Projet Huffman Opti/Pile/Pile.c
+++ Projet Huffman Opti/Pile/Pile.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Pile.h"
 
+void push(Stack**s, Node* val){
+    Stack *nouv = malloc(sizeof(Stack));
+    nouv->no = val;
+    nouv->next = *s;
+    *s = nouv;
+}
+
+Node* pop(Stack** s){
+    Node * nbr = NULL;
+    Stack *temp = *s;
+    if (s!=NULL)
+        {
+            nbr = temp->no;
+            *s = temp->next;
+            free(temp);
+        }
+        return nbr;
+}
