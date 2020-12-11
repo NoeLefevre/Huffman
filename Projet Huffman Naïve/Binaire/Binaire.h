5ff3cb0162ac98f0eff5a926de0053d66c3f91b4
efreims <64196173+efreims@users.noreply.github.com>
2020-12-11 16:31:52 +0100

    Revert "Add files via upload"

---
+++ Projet Huffman Naïve/Binaire/Binaire.h
@@ -0,0 +1,10 @@
+#ifndef BINAIRE_H_INCLUDED
+#define BINAIRE_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../Structure/Structure.h"
+void conversion (FILE* fichier,FILE *fichier2);
+int* binaire(int *tab, int x);
+int nbr_caractere(FILE* fichier);
+#endif // BINAIRE_H_INCLUDED
