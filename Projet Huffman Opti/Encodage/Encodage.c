81863e92a00e8c459be76dc4eaab1335320e82b9
Eric-Wang159 <73349489+Eric-Wang159@users.noreply.github.com>
2020-12-11 16:55:16 +0100

    Update Encodage.c

--- Projet Huffman Opti/Encodage/Encodage.c
+++ Projet Huffman Opti/Encodage/Encodage.c
@@ -4,15 +4,15 @@
 #include "Encodage.h"
 
 
-char* nbr_recherche_arbre_recherche(Node_d *tree,char c)
+char* nbr_recherche_arbre_recherche(Node_d *tree,char c) //Fonction de recherche du code caractère dans un AVL
 {
     if (tree->charactere==c)
     {
-        return tree->code;
+        return tree->code; //Si on trouve le caractère on retourne le code
     }
     else
     {
-        if (tree->charactere<c)
+        if (tree->charactere<c) //Sinon on recherche en fonction du signe, en allant à gauche ou à droite
             tree = tree->right;
         else
             tree = tree->left;
@@ -20,15 +20,15 @@ char* nbr_recherche_arbre_recherche(Node_d *tree,char c)
     }
 }
 
-FILE *encodage(FILE *fichier,Node_d *tree)
+FILE *encodage(FILE *fichier,Node_d *tree) //Tous les codes étant déjà triés dans un AVL, l'encodage et la recherche de caractère sera beaucoup plus simple
 {
     FILE *fichier2=NULL;
     fichier2 = fopen("Encodeur.txt","w+");
     char c=fgetc(fichier);
-    while (c!=EOF)
+    while (c!=EOF) //Tant qu'on a pas atteitn la fin du fichier qu'on veut encoder
     {
-        fprintf(fichier2,"%s",nbr_recherche_arbre_recherche(tree,c));
-        c=fgetc(fichier);
+        fprintf(fichier2,"%s",nbr_recherche_arbre_recherche(tree,c)); //On recherche le code du caractère à encoder grâce à l'AVL et on le print dans le fichier encodage
+        c=fgetc(fichier); //On avance dans le fichier qu'on veut encoder
     }
     return fichier2;
 }
