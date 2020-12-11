e05f6c7f972d6b49298f7c103a648c7fb20e38d0
NoeLefevre <73334531+NoeLefevre@users.noreply.github.com>
2020-12-11 19:22:19 +0100

    Update Decompress.c

--- Projet_Huffman_Naïve/Compress-decompresse/Decompress.c
+++ Projet_Huffman_Naïve/Compress-decompresse/Decompress.c
@@ -8,24 +8,24 @@ FILE* decompress(FILE* fichier,Node *tree)
 {
     char c;
     Node *temp=tree;
-    FILE *trad=NULL;
+    FILE *trad=NULL;//Création du fichier de traduction
     fopen_s(&trad, "trad.txt", "w+");
     do
     {
-        c = fgetc(fichier);
+        c = fgetc(fichier);//On récupère le caractère du fichier encodage
         if (c!=EOF)
         {
             if (c=='1')
-                temp = temp->right;
+                temp = temp->right;//On se déplace à droite
             else
-                temp = temp->left;
-            if (temp->right==NULL && temp->left==NULL)
+                temp = temp->left;//On se déplace à gauche
+            if (temp->right==NULL && temp->left==NULL)//Si on est sur une feuille et donc un caractère à encoder
             {
-                fprintf(trad,"%c",temp->charactere);
-                temp = tree;
+                fprintf(trad,"%c",temp->charactere);//On écrit le caractère correspondant dans le fichier de sortie
+                temp = tree;//On se replace à la racine pour pouvoir décoder d'autres lettres
             }
 
         }
-    } while (c != EOF);
+    } while (c != EOF);//On s'arrête quand on est à la fin du fichier
     return trad;
 }
